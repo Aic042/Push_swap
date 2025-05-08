766bd581915ae37ea5c55df96491baa560fc04bf
Alvaro Ingunza Curro <aingunza@c2r3s6.42madrid.com>
2025-05-08 17:00:15 +0200

    Revert "You must solve th e error with  strings with whitespaces only. Check grok"
    This reverts commit fa70e064a52d4d8110bbb0a27d6eddda6e793b09.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aingunza <aingunza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:21:55 by root              #+#    #+#             */
-/*   Updated: 2025/05/08 16:57:46 by aingunza         ###   ########.fr       */
+/*   Updated: 2025/05/08 16:04:03 by aingunza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,6 @@ int last_parse_args(char **argv)
 	}
 	else
 		return (1);
-
 }
 
 int	main(int argc, char **argv)
@@ -102,5 +101,39 @@ int	main(int argc, char **argv)
 // 	return (0);
 // }
 
-//./push_swap "     " 3 2 1 "    "
-// error with this output
+// % ./push_swap "-3 1   '-6'  "
+
+// ./push_swap +-000009 -1
+// Error
+// sa
+
+
+
+
+
+// % valgrind --leak-check=full  ./push_swap 2343 5 23 65 5
+// ==1552113== Memcheck, a memory error detector
+// ==1552113== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+// ==1552113== Using Valgrind-3.18.1 and LibVEX; rerun with -h for copyright info
+// ==1552113== Command: ./push_swap 2343 5 23 65 5
+// ==1552113== 
+// Error
+// ==1552113== 
+// ==1552113== HEAP SUMMARY:
+// ==1552113==     in use at exit: 20 bytes in 1 blocks
+// ==1552113==   total heap usage: 18 allocs, 17 frees, 189 bytes allocated
+// ==1552113== 
+// ==1552113== 20 bytes in 1 blocks are definitely lost in loss record 1 of 1
+// ==1552113==    at 0x4848899: malloc (in /usr/libexec/valgrind/vgpreload_memcheck-amd64-linux.so)
+// ==1552113==    by 0x40150E: parse_args (pars_utils.c:89)
+// ==1552113==    by 0x401D5B: main (push_swap.c:73)
+// ==1552113== 
+// ==1552113== LEAK SUMMARY:
+// ==1552113==    definitely lost: 20 bytes in 1 blocks
+// ==1552113==    indirectly lost: 0 bytes in 0 blocks
+// ==1552113==      possibly lost: 0 bytes in 0 blocks
+// ==1552113==    still reachable: 0 bytes in 0 blocks
+// ==1552113==         suppressed: 0 bytes in 0 blocks
+// ==1552113== 
+// ==1552113== For lists of detected and suppressed errors, rerun with: -s
+// ==1552113== ERROR SUMMARY: 1 errors from 1 contexts (suppressed: 0 from 0)
